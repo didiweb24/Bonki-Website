8fad19302700732ed5e22a226049dd08bfc66264
didiweb24 <oktaydilan1@gmail.com>
2022-08-21 11:42:02 +0300

    mobilemenu state eklendi

--- my-app/src/commons/header/Navbar.jsx
+++ my-app/src/commons/header/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [MobileMenu, setMobileMenu] = useState(false);
   return (
     <>
       <header className="header">
@@ -34,6 +35,13 @@ const Navbar = () => {
               <Link to="/contact">contact</Link>
             </li>
           </ul>
+          <button className="toggle" onClick={() => setMobileMenu(!MobileMenu)}>
+            {MobileMenu ? (
+              <i className="fas fa-times close home-bth"></i>
+            ) : (
+              <i className="fas fa-bars open"></i>
+            )}
+          </button>
         </div>
       </header>
     </>
